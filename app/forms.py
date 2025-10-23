ca315244cc3fdcac13762bec36ebb0f5fa60673a
RyanDuong0 <ryan.duong@aol.co.uk>
2025-10-23 17:18:51 +0100

    Fixed password and confirmPassword to be PasswordField rather than StringField

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.fields.simple import PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
-
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -10,10 +9,12 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Log In')
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), Length(3, 30)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = StringField('Password', validators=[DataRequired()])
-    confirmPassword = StringField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    bio = StringField('Bio', validators=[DataRequired()])
+    submit = SubmitField('Register')
 
 class LogoutForm(FlaskForm):
     submit = SubmitField('Log Out')
