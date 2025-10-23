68027f50ee333d2fc43f625cafb614a65827df8c
RyanDuong0 <ryan.duong@aol.co.uk>
2025-10-23 17:19:34 +0100

    Added CSRF token

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_wtf import CSRFProtect
+
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    csrf.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
