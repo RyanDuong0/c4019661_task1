18407abe0f93e589477d30c68dce8f5ef527638d
RyanDuong0 <ryan.duong@aol.co.uk>
2025-10-23 17:20:47 +0100

    Added dashboard, registration and login methods

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .forms import RegisterForm, LoginForm, LogoutForm
+from flask_login import login_required, user_login_confirmed
 
 main = Blueprint('main', __name__)
 
@@ -6,7 +8,33 @@ main = Blueprint('main', __name__)
 def home():
     return redirect(url_for('main.register'))
 
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        if username == "admin" and password == "password123":
+            return redirect(url_for('main.dashboard'))
+        else:
+            flash("Invalid username or password", "danger")
+
+    return render_template("login.html", form=form)
+
 @main.route('/register', methods=['GET', 'POST'])
 def register():
-    name = request.form.get('name') if request.method == 'POST' else ''
-    return render_template('register.html', name=name)
+    form = RegisterForm()
+    name = None
+    if form.validate_on_submit():
+        name = form.username.data
+        return render_template("register.html", form=form, name=name)
+
+    return render_template('register.html', form=form,name=name)
+
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    form = LogoutForm()
+    user = request.args.get('user', 'Guest')
+    return render_template('dashboard.html', user=user, form=form)
