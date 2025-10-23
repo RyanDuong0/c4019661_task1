6af1089bdc373523b7ef21a059cb1040a6e8dc1a
RyanDuong0 <ryan.duong@aol.co.uk>
2025-10-23 17:55:31 +0100

    Added parameters to validator in RegisterForm to raise error messages

--- app/forms.py
+++ app/forms.py
@@ -4,9 +4,12 @@ from wtforms.fields.simple import PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class RegisterForm(FlaskForm):
-    name = StringField('Username', validators=[DataRequired(), Length(3, 30)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    name = StringField('Username', validators=[
+        DataRequired(message="Username is required."),
+        Length(3, 30, message="Username must be between 3 and 30 characters")
+    ])
+    email = StringField('Email', validators=[DataRequired(message="Email is required."), Email()])
+    password = PasswordField('Password', validators=[DataRequired("Password is required.")])
+    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired("Please confirm your password."), EqualTo('password')])
     bio = StringField('Bio', validators=[DataRequired()])
     submit = SubmitField('Register')
